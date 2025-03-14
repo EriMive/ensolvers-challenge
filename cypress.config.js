2984777632bd328d3810fcb9fbc404a06c10c863
emv <ermive33@gmail.com>
2025-03-14 14:09:50 -0500

    Cypress tests with visual reports and updated configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,28 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: '2eth9c',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implementa los listeners aquí
     },
+    specPattern: 'cypress/e2e/**/*.cy.js',
   },
-});
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  env: {
+    baseUrl: 'https://qa-challenge.ensolvers.com',
+  },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  browser: 'chrome',
+  screenshotOnRunFailure: true,
+})
